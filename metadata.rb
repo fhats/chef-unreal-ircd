311354b8de31918ee300c739aec23eda0cd48626
Fred Hatfull <fred.hatfull@gmail.com>
2014-08-10 17:26:36 -0700

    Ports over most of the current IRCd options to generate some prety naive configs

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-name             'chef-unreal-ircd'
-maintainer       ''
-maintainer_email ''
-license          ''
+name             'unreal-ircd'
+maintainer       'Fred Hatfull (fhats)'
+maintainer_email 'fred.hatfull@gmail.com'
+license          'MIT'
 description      'Installs/Configures chef-unreal-ircd'
 long_description 'Installs/Configures chef-unreal-ircd'
 version          '0.1.0'
 
+depends 'build-essential'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,37 @@
 #
-# Cookbook Name:: chef-unreal-ircd
+# Cookbook Name:: unreal-ircd
 # Recipe:: default
 #
-# Copyright (C) 2014 
+# Copyright (C) 2014 Fred Hatfull
 #
 # 
 #
+
+user node['unreal']['user'] do
+  comment "Unreal IRCd user"
+  only_if { node['unreal']['create_user'] }
+  shell "/bin/false"
+  system true
+end
+
+directory ::File.dirname(node['unreal']['pidfile']) do
+  owner    node['unreal']['user']
+end
+
+directory ::File.dirname(node['unreal']['tunefile']) do
+  owner    node['unreal']['user']
+end
+
+include_recipe "unreal-ircd::source"
+
+template "#{node['unreal']['unreal_directory']}/unrealircd.conf" do
+  source "unrealircd.conf.erb"
+  owner  node['unreal']['user']
+  mode   "0644"
+end
+
+directory "#{node['unreal']['unreal_directory']}/unrealircd.conf.d" do
+  owner node['unreal']['user']
+  recursive true
+end
+
